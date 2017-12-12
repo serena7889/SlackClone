e98a81f1fc52402dd8ac282b5e42e9fba0b1ab92
Serena <serenalambert1731@gmail.com>
2017-12-12 14:44:28 +0400

    end of lesson 4

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Serena Lambert on 12/12/2017.
+//  Copyright © 2017 Serena Lambert. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Serena Lambert on 12/12/2017.
+//  Copyright © 2017 Serena Lambert. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
