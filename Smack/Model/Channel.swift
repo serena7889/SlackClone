e04834df80676bf3db7d782ccde014d878c5a6ea
Serena <serenalambert1731@gmail.com>
2018-01-11 11:15:37 +0400

    lesson -19

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,23 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Serena Lambert on 11/01/2018.
+//  Copyright © 2018 Serena Lambert. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String
+    public private(set) var channelDescription: String
+    public private(set) var id: String
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Smack/Services/MessageService.swift
@@ -0,0 +1,53 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Serena Lambert on 11/01/2018.
+//  Copyright © 2018 Serena Lambert. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannels(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: HEADER_BEARER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                do {
+                    if let json = try JSON(data: data).array {
+                        for item in json {
+                            let name = item["name"].stringValue
+                            let description = item["description"].stringValue
+                            let id = item["_id"].stringValue
+                            let channel = Channel(channelTitle: name, channelDescription: description, id: id)
+                            self.channels.append(channel)
+                        }
+                    }
+                } catch {
+                    debugPrint(error)
+                }
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
